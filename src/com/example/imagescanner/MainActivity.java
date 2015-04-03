796417b7d15bedba5eeea5d4f0860f5e5e13a659
DarkHorsePXF <darkhorsepxf@gmail.com>
2015-04-03 17:44:44 +0800

    add animation when you are switching the ViewPager

--- src/com/example/imagescanner/MainActivity.java
+++ src/com/example/imagescanner/MainActivity.java
@@ -9,8 +9,11 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -35,10 +38,10 @@ import android.widget.ListAdapter;
 public class MainActivity extends ActionBarActivity {
 	GridView gvImages;
 	Context context;
-	ArrayList<HashMap<String, Object>> picList;
+	static ArrayList<HashMap<String, Object>> picList;
 	Handler handler;
+	AlertDialog showImageAlert;
 	
-	ImageLoader imageLoader=ImageLoader.getInstance(); //step 1:get ImageLoader
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,38 +56,23 @@ public class MainActivity extends ActionBarActivity {
 		picList=new ArrayList<>();
 		getImagesList();
 		
-		imageLoader.init(ImageLoaderConfiguration	//step 2:init the ImageLoader
-				.createDefault(context));			
-		
-		
 		gvImages=(GridView) findViewById(R.id.gvImages);
 		gvImages.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				showImage(view,position);
+				showImage(position);
 			}
 
-			private void showImage(View view, int position) {
-				String path=picList.get(position).get("path").toString();
-				Bitmap bitmap=BitmapFactory.decodeFile(path);
-				((ImageView)view).setImageBitmap(bitmap);
+			private void showImage(int position) {
+				Intent intent=new Intent(context, ShowImageActivity.class);
+				intent.putExtra(ShowImageActivity.EXTRA_IMAGE_INDEX, position);
+				startActivity(intent);
 			}
 		});
-		/*handler=new Handler(){
-
-			@Override
-			public void handleMessage(Message msg) {
-				if(msg.what==ImagesAdapter.REFRESH_IMAGE){
-					ImageView iv=((ViewHolder)msg.obj).iv;
-					Bitmap bm=((ViewHolder)msg.obj).bm;
-					iv.setImageBitmap(bm);
-				}
-			}
-		};
-		*/
-		Adapter adapter=new ImagesAdapter(context,picList);
+		
+		Adapter adapter=new ImageLoaderAdapter(context,picList);
 		gvImages.setAdapter((ListAdapter) adapter);
 		
 	}
@@ -125,99 +113,7 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 	
-	public class ImagesAdapter extends BaseAdapter implements Adapter {
-		//ExecutorService executorService=Executors.newFixedThreadPool(2);
-		Context mContext;
-		ArrayList<HashMap<String, Object>> picList;
-		ViewHolder holder;
-		public static final int REFRESH_IMAGE=0x123;
-		
-		
-		
-		
-		
-		DisplayImageOptions options;
-		
-		public ImagesAdapter(Context context,ArrayList<HashMap<String, Object>> picList) {
-			
-			options=new DisplayImageOptions.Builder()	//step3:set options of ImageLoader
-			.cacheInMemory(true)	
-			.cacheOnDisk(true)
-			.considerExifParams(true)
-			.showImageOnLoading(R.drawable.ic_stub)
-			.showImageOnFail(R.drawable.ic_error)
-			.showImageForEmptyUri(R.drawable.ic_empty)
-			.build();
-			
-			
-			this.mContext=context;
-			this.picList=picList;
-		}
-		@Override
-		public int getCount() {
-			return picList.size();
-		}
-
-		@Override
-		public Object getItem(int position) {
-			return picList.get(position);
-		}
-
-		@Override
-		public long getItemId(int position) {
-			return position;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			if(convertView==null){
-				convertView=LayoutInflater.from(mContext).inflate(R.layout.layout_image_content, null);
-				holder=new ViewHolder();
-				holder.iv=(ImageView) convertView.findViewById(R.id.ivImage);
-				convertView.setTag(holder);
-			}
-			final String path=(String) picList.get(position).get("path");
-			holder=(ViewHolder) convertView.getTag();
-			holder.iv.setImageResource(R.drawable.ic_launcher);
-			
-			/*ignore the fail code
-			final int pst=position;
-			try {
-				executorService.execute(new Runnable() {
-					
-					@Override
-					public void run() {
-						Bitmap bitmap=BitmapUtil.tryGetBitmap(path, 100000, 100000);
-						holder.bm=bitmap;
-						Message msg=new Message();
-						msg.what=REFRESH_IMAGE;
-						msg.arg1=pst;
-						msg.obj=holder;
-						handler.sendMessage(msg);
-					}
-				});
-				System.out.println(path);
-			} catch (OutOfMemoryError e) {
-				
-				AlertUtil.alert(mContext, "内存爆啦~~~加载失败！");
-			}
-			*/
-			imageLoader.displayImage(
-					"file:/"+path, 	//URL:	local files-->"file:/"
-									//		Internet files-->"http://"
-									//		contentProvider-->"content:/"
-									//		drawable files-->"drawable:/"
-									
-					holder.iv, 		//display on this imageView 
-					options);		//displayed options
-			return convertView;
-		}
-		class ViewHolder{
-			ImageView iv;
-			Bitmap bm;
-		}
-
-	}
+	
 	
 	
 }
